76f572b052c57623fbd02212ca59e621265e1bc7
dafeys <dafeys@outlook.com>
2023-07-23 00:51:13 +0300

    3 lvl

---
+++ app/models/question.rb
@@ -0,0 +1,8 @@
+class Question < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 40}
+  validates :body, presence: true, length: { minimum: 3, maximum: 1000}
+
+  def format_created_at
+    self.created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
+end
